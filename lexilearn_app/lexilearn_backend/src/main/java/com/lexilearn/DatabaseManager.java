44514f42bc5608001ea1dedf2ac06b701d361637
Şule <209013856+krlpsule@users.noreply.github.com>
2026-04-16 15:37:05 +0300

    Fixing backend url

--- lexilearn_app/lexilearn_backend/src/main/java/com/lexilearn/DatabaseManager.java
+++ lexilearn_app/lexilearn_backend/src/main/java/com/lexilearn/DatabaseManager.java
@@ -1,11 +1,13 @@
 package com.lexilearn;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
     // Update these with your local MySQL credentials
-    private static final String URL = "jdbc:mysql://mysql-22586bd6-lexilearnapp.a.aivencloud.com:12421/defaultdb?sslMode=REQUIRED";
+    private static final String URL = "jdbc:mysql://avnadmin:AVNS_gSWs219QI_R5gSyb8_F@mysql-22586bd6-lexilearnapp.a.aivencloud.com:12421/defaultdb?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    //
     private static final String USER = "avnadmin";
     private static final String PASSWORD = "AVNS_gSWs219QI_R5gSyb8_F";
 
